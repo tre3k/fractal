cdb3b7f01d755484259118ad37531cae3319f778
Kirill Pshenichnyi <pshcyrill@mail.ru>
2022-05-16 14:29:55 +0300

    add gnuplot script for approximate averages data, and script for generate half sphere

---
+++ generateHalfSphere.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import math as m
+
+X = 2048
+Y = 2048
+
+data = [0] * X
+for i in range(X):
+    data[i] = [0] * Y
+
+def printData():
+    fw = open("half-sphere-"+str(X)+"x"+str(Y)+".txt",'w')
+    print(X,"x",Y)
+    print("to file:","half-sphere-"+str(X)+"x"+str(Y)+".txt")
+    fw.write(str(X)+'\n'+str(Y)+'\n')
+    for i in range(0,X):
+        for j in range(0,Y):
+            fw.write(str(data[i][j])+'\n')
+    return
+
+
+radius = 512
+
+for i in range(X):
+    for j in range(Y):
+        x = i - X/2
+        y = j - Y/2
+        core =  radius**2 - x**2 - y**2
+        if core >= 0:
+            data[i][j] = m.sqrt(core)
+
+printData()
